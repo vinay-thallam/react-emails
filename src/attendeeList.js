896807c69ccfafbf8032881ee06834fdcce87f6d
vinay thallam <vinay.rgit@gmail.com>
2019-09-26 14:52:48 +0530

    react component rendering in preview or send mail mode

--- src/attendeeList.js
+++ src/attendeeList.js
@@ -5,7 +5,7 @@ export default function AttendeeList ({
   mode,
   attributes
 }) {
-  return (
+  return mode === 'Edit' ? (
     <table 
       className='att-section-table' 
       width='100%' 
@@ -111,6 +111,121 @@ export default function AttendeeList ({
           </table>
         </td>
       </tr>
-    </table>
+    </table> 
+  ) : (
+    <table 
+    className='att-section-table' 
+    width='100%' 
+    cellspacing='0' 
+    cellpadding="0" 
+    style={{
+      borderTop: '1px solid #ECEBEA', 
+      borderBottom: '1px solid #ECEBEA',
+      padding: '0 0 8px'
+    }}
+  >
+    <tr>
+      <td 
+        className='att-section-col'
+        width='50%' 
+        style={{
+          padding: '24px 20px 0 0'
+        }}
+      >
+        <table 
+          className='att-table' 
+          width='100%' 
+          cellspacing='0'
+          cellpadding='0'
+        >
+          <thead>
+            <tr>
+              <th 
+                width='100%' 
+                style={{
+                  textAlign: 'left',
+                  color: '#706E6B',
+                  fontSize: '12px', 
+                  lineHeight: '17px', 
+                  fontWeight: 'normal', 
+                  padding: '0 0 16px',
+                  letterSpacing: '1px'
+                }}
+              >{attributes.heading}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td 
+                width='100%'
+              >
+                {'{{#each internal_attendees}}'}
+                  <table 
+                    width='100%' 
+                    cellspacing='0'
+                    cellpadding='0'
+                  >
+                    <tr>
+                      <td 
+                        width='100%' 
+                        style={{
+                          fontSize: '12px',
+                          fontWeight: '600', 
+                          color: '#2B2826',
+                          lineHeight: '13px'
+                        }}
+                      >
+                        {attributes.attendee_line_1.reduce((acc, value) => {
+                          return `${acc} {{${value}}}`
+                        }, '')}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td 
+                        width="100%" 
+                        style={{
+                          fontSize: '11px', 
+                          color: '#706E6B', 
+                          lineHeight: '13px'
+                        }}
+                      >
+                          <a 
+                            href="mailto:bessie.mc@acmecorp.com" 
+                            target="_blank" 
+                            style={{
+                              color: '#706E6B',
+                              textDecoration: 'none'
+                            }}
+                          >
+                            {attributes.attendee_line_2.reduce((acc, value) => {
+                              return `${acc} {{${value}}}`
+                            }, '')}
+                          </a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td 
+                        width="100%" 
+                        style={{
+                          fontSize: '11px', 
+                          color: '#706E6B', 
+                          lineHeight: '13px', 
+                          padding: '0 0 16px',
+                        }}
+                      >
+                        {attributes.attendee_line_3.reduce((acc, value) => {
+                              return `${acc} {{${value}}}`
+                        }, '')}
+                      </td>
+                    </tr>
+                  </table>
+                {'{{/each}}'}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+  </table> 
   )
 }
---
+++ src/dummyProps.js
@@ -0,0 +1,11 @@
+export const attendeeListProps = {
+    mode : 'Edit',
+    attributes : {
+        heading : 'Participants',
+        attendee_line_1 : ['first_name', 'last_name'],
+        attendee_line_2 : ['email'],
+        attendee_line_3 : ['company'],
+    }
+}
+
+
